b69f836732b1a553de89c7d1f3e78f7274632aa1
admin <depdios10@gmail.com>
2026-02-19 23:35:02 -0600

    hygfd

--- scripts/test.lua
+++ scripts/test.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/20278bdddd2487776d40c81c2f3535cf8d797f85e6ea8d9fccc5866991a696de/download"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ltseverydayyou/uuuuuuu/refs/heads/main/FixShitChatSystem.lua"))()
