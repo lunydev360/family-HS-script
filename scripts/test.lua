fa8c267b7c407cfc58c3728a6b5c2507f1c0ab94
admin <depdios10@gmail.com>
2026-02-20 15:01:19 -0600

    ugf

--- scripts/test.lua
+++ scripts/test.lua
@@ -1,3 +1,1 @@
--- [[ CHECK THE DESCRIPTION FOR OTHER SOURCES ]]--
---[[ Ill just put the arsenal script keyless and source in here ]]--
-loadstring(game:HttpGet("https://raw.githubusercontent.com/RiseValco/keybypasses/main/devhub/Arsenal.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wa0101/Roblox-ESP/refs/heads/main/esp.lua",true))()
