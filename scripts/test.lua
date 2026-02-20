5eb90d9dc7cbd7a9395149dd7ae3c10f14a6209a
admin <depdios10@gmail.com>
2026-02-19 23:31:27 -0600

    hfdhf

--- scripts/test.lua
+++ scripts/test.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ShadowBey01/SHWX-Team-Rise-Scripts/refs/heads/main/RiseChat.lua"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/20278bdddd2487776d40c81c2f3535cf8d797f85e6ea8d9fccc5866991a696de/download"))()
