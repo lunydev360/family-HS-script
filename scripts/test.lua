f4c621ae0022c8a7a4a34ea90018344644a910e3
admin <depdios10@gmail.com>
2026-02-20 15:21:17 -0600

    ugf

--- scripts/test.lua
+++ scripts/test.lua
@@ -1,150 +1,88 @@
---[[
-	WARNING: Heads up! This script has not been verified by ScriptBlox. Use at your own risk!
-]]
---//Toggle\\--
-getgenv().Toggle = true -- This toggles the esp, turning it to false will turn it off
-getgenv().TC = false -- This toggles team check, turning it on will turn on team check
-local PlayerName = "Name" -- You can decide if you want the Player's name to be a display name which is "DisplayName", or username which is "Name"
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/deeeity/mercury-lib/master/src.lua"))()
 
---//Variables\\--
-local P = game:GetService("Players")
-local LP = P.LocalPlayer
+local gui = Library:create{
+    Theme = Library.Themes.Serika
+}
 
---//Debounce\\--
-local DB = false
+local tab = gui:tab{
+    Icon = "rbxassetid://6034996695",
+    Name = "Aimbot"
+}
 
---//Notification\\--
-game.StarterGui:SetCore("SendNotification", {
-	Title = "Notification",
-	Text = "This Script Was Made By Psygen" ,
-	Button1 = "Ok",
-	Button2 = "Shut Up",
-	Duration = math.huge
+tab:button({
+    Name = "show prompt",
+    Callback = function()
+        tab:prompt{
+            Title = "baby",
+            Text = "shark doo doo doo doo im blank lmao",
+            Buttons = {
+                Ok = function()
+                    tab:prompt{
+                        Followup = true,
+                        Title = "really?",
+                        Text = "you sure?=",
+                        Buttons = {
+                            Yes = function()
+                                tab:prompt{
+                                    Followup = true,
+                                    Title = "xd",
+                                    Text = "sus",
+                                    Buttons = {
+                                        balls = function()
+                                            gui:set_status("github")
+                                        end,
+                                        anal = function()
+                                            gui:set_Status("money")
+                                        end
+                                    }
+                                }
+                            end,
+                        }
+                    }
+                end,
+            }
+        }
+    end,
 })
-
-game.StarterGui:SetCore("SendNotification", {
-	Title = "Notification",
-	Text = "Please Subscribe To Psygen On Youtube" ,
-	Button1 = "Ok",
-	Button2 = "Shut Up",
-	Duration = math.huge
+tab:keybind({Callback = function()
+    gui:prompt()
+end,})
+tab:dropdown({
+    Name = "Dropdown",
+    Description = "yeeeeeeeeeeeeeeeeeeeboi",
+    StartingText = "Bodypart",
+    Items = {
+        "Head",
+        "Torso",
+        "Random"
+    }
 })
+tab:dropdown({
+    Name = "yes",
+    StartingText = "Number",
+    Items = {
+        {"One", 1},
+        {"Two", 2},
+        {"Three", 3}
+    },
+    Description = "amongu s",
+    Callback = function(v)
+        print(v, "clicked")
+    end,
+})
+local cum = tab:slider({Callback = function(v)
+    gui:set_status(v)
+end})
 
---//Loop\\--
-while task.wait() do
-	if not getgenv().Toggle then
-		break
-	end
-	if DB then 
-		return 
-	end
-	DB = true
+tab:textbox({Callback = function(v)
+    gui:prompt{Text = v}
+end,})
 
-	pcall(function()
-		for i,v in pairs(P:GetChildren()) do
-			if v:IsA("Player") then
-				if v ~= LP then
-					if v.Character then
-
-						local pos = math.floor(((LP.Character:FindFirstChild("HumanoidRootPart")).Position - (v.Character:FindFirstChild("HumanoidRootPart")).Position).magnitude)
-						-- Credits to Infinite Yield for this part (pos) ^^^^^^
-
-						if v.Character:FindFirstChild("Totally NOT Esp") == nil and v.Character:FindFirstChild("Icon") == nil and getgenv().TC == false then
-							--//ESP-Highlight\\--
-							local ESP = Instance.new("Highlight", v.Character)
-
-							ESP.Name = "Totally NOT Esp"
-							ESP.Adornee = v.Character
-							ESP.Archivable = true
-							ESP.Enabled = true
-							ESP.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-							ESP.FillColor = v.TeamColor.Color
-							ESP.FillTransparency = 0.5
-							ESP.OutlineColor = Color3.fromRGB(255, 255, 255)
-							ESP.OutlineTransparency = 0
-
-							--//ESP-Text\\--
-							local Icon = Instance.new("BillboardGui", v.Character)
-							local ESPText = Instance.new("TextLabel")
-
-							Icon.Name = "Icon"
-							Icon.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-							Icon.Active = true
-							Icon.AlwaysOnTop = true
-							Icon.ExtentsOffset = Vector3.new(0, 1, 0)
-							Icon.LightInfluence = 1.000
-							Icon.Size = UDim2.new(0, 800, 0, 50)
-
-							ESPText.Name = "ESP Text"
-							ESPText.Parent = Icon
-							ESPText.BackgroundColor3 = v.TeamColor.Color
-							ESPText.BackgroundTransparency = 1.000
-							ESPText.Size = UDim2.new(0, 800, 0, 50)
-							ESPText.Font = Enum.Font.SciFi
-							ESPText.Text = v[PlayerName].." | Distance: "..pos
-							ESPText.TextColor3 = v.TeamColor.Color
-							ESPText.TextSize = 18.000
-							ESPText.TextWrapped = true
-						else
-							if v.TeamColor ~= LP.TeamColor and v.Character:FindFirstChild("Totally NOT Esp") == nil and v.Character:FindFirstChild("Icon") == nil and getgenv().TC == true then
-								--//ESP-Highlight\\--
-								local ESP = Instance.new("Highlight", v.Character)
-
-								ESP.Name = "Totally NOT Esp"
-								ESP.Adornee = v.Character
-								ESP.Archivable = true
-								ESP.Enabled = true
-								ESP.DepthMode = Enum.HighlightDepthMode.AlwaysOnTop
-								ESP.FillColor = v.TeamColor.Color
-								ESP.FillTransparency = 0.5
-								ESP.OutlineColor = Color3.fromRGB(255, 255, 255)
-								ESP.OutlineTransparency = 0
-
-								--//ESP-Text\\--
-								local Icon = Instance.new("BillboardGui", v.Character)
-								local ESPText = Instance.new("TextLabel")
-
-								Icon.Name = "Icon"
-								Icon.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-								Icon.Active = true
-								Icon.AlwaysOnTop = true
-								Icon.ExtentsOffset = Vector3.new(0, 1, 0)
-								Icon.LightInfluence = 1.000
-								Icon.Size = UDim2.new(0, 800, 0, 50)
-
-								ESPText.Name = "ESP Text"
-								ESPText.Parent = Icon
-								ESPText.BackgroundColor3 = v.TeamColor.Color
-								ESPText.BackgroundTransparency = 1.000
-								ESPText.Size = UDim2.new(0, 800, 0, 50)
-								ESPText.Font = Enum.Font.SciFi
-								ESPText.Text = v[PlayerName].." | Distance: "..pos
-								ESPText.TextColor3 = v.TeamColor.Color
-								ESPText.TextSize = 18.000
-								ESPText.TextWrapped = true
-							else
-								if not v.Character:FindFirstChild("Totally NOT Esp").FillColor == v.TeamColor.Color and not v.Character:FindFirstChild("Icon").TextColor3 == v.TeamColor.Color then
-									v.Character:FindFirstChild("Totally NOT Esp").FillColor = v.TeamColor.Color
-									v.Character:FindFirstChild("Icon").TextColor3 = v.TeamColor.Color
-								else
-									if v.Character:FindFirstChild("Totally NOT Esp").Enabled == false and v.Character:FindFirstChild("Icon").Enabled == false then
-										v.Character:FindFirstChild("Totally NOT Esp").Enabled = true
-										v.Character:FindFirstChild("Icon").Enabled = true
-									else
-										if v.Character:FindFirstChild("Icon") then
-											v.Character:FindFirstChild("Icon")["ESP Text"].Text = v[PlayerName].." | Distance: "..pos
-										end
-									end
-								end
-							end
-						end
-					end
-				end
-			end
-		end
-	end)
-
-	wait()
-
-	DB = false
-end
+tab:color_picker({
+    Name = "your mom's color",
+    Style = Library.ColorPickerStyles.Legacy,
+    Description = "Click to adjust color...",
+    Callback = function(color)
+        print(color)
+    end,
+})
